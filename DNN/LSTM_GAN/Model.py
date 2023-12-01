69995e1cdd3a33002855710df6c5a61232e6a4fb
Mohammad Hossein Karam Sichani <m.hossein.karam.s@gmail.com>
2023-12-01 15:04:50 +0330

    .

--- DNN/LSTM_GAN/Model.py
+++ DNN/LSTM_GAN/Model.py
@@ -1,85 +1,192 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import LSTM, Dense, Input, Reshape, Flatten
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import plot_model
+
+gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
 # Load and preprocess your input data
 data = pd.read_csv('/home/mhossein/my_projects/Forex_DNN/Data/GBPUSD_1d_2.csv')
-# Preprocess and normalize the data as required
+features = data[['Open', 'High', 'Low', 'Close', 'Vol']].values
 
-# Define the number of past time steps to consider
-num_timesteps = 32
+# Normalize the data
+scaler = MinMaxScaler()
+scaled_features = scaler.fit_transform(features)
 
-# Create input and output sequences
-input_seq = []
-output_seq = []
-for i in range(len(data) - num_timesteps - 1):
-    input_seq.append(data.iloc[i : i + num_timesteps][[ 'Open','High', 'Low','Close' , 'Vol']].values)
-    output_seq.append(data.iloc[i + num_timesteps][[ 'Open','High', 'Low','Close' , 'Vol']].values)  # Assuming output is the next data point
+# Define the sequence length for LSTM input
+sequence_length = 24  # You can adjust this value depending on your data
 
-# Convert the input and output sequences to numpy arrays
-input_seq = np.array(input_seq)
-output_seq = np.array(output_seq)
+# Create sequences of input and output data for LSTM training
+sequences = []
+next_closes = []
+for i in range(len(scaled_features) - sequence_length - 1):
+    seq = scaled_features[i:i + sequence_length]
+    target = scaled_features[i + sequence_length + 1][3]  # Predicting the 'Close' price
+    sequences.append(seq)
+    next_closes.append(target)
+
+# Convert the sequences and next_closes to numpy arrays
+X = np.array(sequences)
+y = np.array(next_closes)
 
 # Split the data into training and testing sets
-train_size = int(len(input_seq) * 0.8)
-train_input = input_seq[:train_size]
-train_output = output_seq[:train_size]
-test_input = input_seq[train_size:]
-test_output = output_seq[train_size:]
+train_size = int(0.8 * len(X))
+X_train, X_test = X[:train_size], X[train_size:]
+y_train, y_test = y[:train_size], y[train_size:]
 
-# Build the generator model
+# Build the LSTM model
+lstm_model = Sequential()
+lstm_model.add(LSTM(64, input_shape=(sequence_length, features.shape[1])))
+lstm_model.add(Dense(1, activation='linear'))
+
+# Compile the LSTM model
+lstm_model.compile(loss='mean_squared_error', optimizer='adam')
+
+# Train the LSTM model
+lstm_model.fit(X_train, y_train, epochs=50, batch_size=64, validation_data=(X_test, y_test))
+
+# Build the GAN generator
+latent_dim = 100  # Dimension of the GAN latent space
+
 generator = Sequential()
-generator.add(LSTM(units=128, return_sequences=True, input_shape=(num_timesteps, input_seq.shape[2])))
-# Add more LSTM layers or other layers as needed
-generator.add(Dense(units=output_seq.shape[1], activation='sigmoid'))
+generator.add(Dense(128, input_dim=latent_dim))
+generator.add(Dense(sequence_length * features.shape[1], activation='tanh'))
+generator.add(Reshape((sequence_length, features.shape[1])))
 
-# Build the discriminator model
+# Build the GAN discriminator
 discriminator = Sequential()
-discriminator.add(LSTM(units=128, return_sequences=True, input_shape=(num_timesteps, output_seq.shape[1])))
-# Add more LSTM layers or other layers as needed
-discriminator.add(Dense(units=1, activation='sigmoid'))
+discriminator.add(Flatten(input_shape=(sequence_length, features.shape[1])))
+discriminator.add(Dense(128, activation='relu'))
+discriminator.add(Dense(1, activation='sigmoid'))
 
-# Compile the generator and discriminator models
-generator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001))
-discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001))
+# Compile the discriminator
+discriminator.compile(loss='binary_crossentropy', optimizer='adam')
 
-# Combine the generator and discriminator into a GAN model
+# Build the GAN model
 discriminator.trainable = False
-gan_input = keras.Input(shape=(num_timesteps, input_seq.shape[2]))
+gan_input = Input(shape=(latent_dim,))
 gan_output = discriminator(generator(gan_input))
-gan = keras.Model(gan_input, gan_output)
-gan.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001))
+gan = Model(gan_input, gan_output)
 
-# Training loop
-batch_size = 32
-epochs = 100
-for epoch in range(epochs):
-    for batch in range(len(train_input) // batch_size):
-        # Train the discriminator
-        real_data = train_output[batch * batch_size : (batch + 1) * batch_size]
-        fake_data = generator.predict(train_input[batch * batch_size : (batch + 1) * batch_size])
-        discriminator.trainable = True
-        discriminator.train_on_batch(real_data, np.ones((batch_size, 1)))
-        discriminator.train_on_batch(fake_data, np.zeros((batch_size, 1)))
+# Compile the GAN model
+gan.compile(loss='binary_crossentropy', optimizer='adam')
 
-        # Train the generator
-        discriminator.trainable = False
-        gan.train_on_batch(train_input[batch * batch_size : (batch + 1) * batch_size], np.ones((batch_size, 1)))
+# Function to train the GAN
+def train_gan(epochs=1, batch_size=128):
+    for e in range(epochs):
+        for _ in range(len(X_train) // batch_size):
+            # Train the discriminator
+            noise = np.random.normal(0, 1, size=[batch_size, latent_dim])
+            generated_data = generator.predict(noise)
+            real_data = X_train[np.random.randint(0, len(X_train), size=batch_size)]
 
-    # Print the current epoch and loss
-    print(f"Epoch: {epoch+1}/{epochs}, Loss: {gan.evaluate(test_input, np.ones((len(test_input), 1)))}")
+            X_batch = np.concatenate([real_data, generated_data])
+            y_dis = np.zeros(2 * batch_size)
+            y_dis[:batch_size] = 0.9  # One-sided label smoothing for the discriminator
 
-# Use the trained generator for predictions
-predictions = generator.predict(test_input)
+            discriminator.trainable = True
+            d_loss = discriminator.train_on_batch(X_batch, y_dis)
 
-# Evaluate the performance of the model
-# Add your evaluation metrics for forex trend prediction
+            # Train the generator (combined with the discriminator)
+            noise = np.random.normal(0, 1, size=[batch_size, latent_dim])
+            y_gen = np.ones(batch_size)
 
-# Save the trained models if needed
-generator.save("generator_model.h5")
-discriminator.save("discriminator_model.h5")
+            discriminator.trainable = False
+            g_loss = gan.train_on_batch(noise, y_gen)
+
+        print(f"Epoch {e + 1}, Discriminator Loss: {d_loss}, Generator Loss: {g_loss}")
+
+# Train the GAN
+train_gan(epochs=50, batch_size=64)
+
+# Function to generate synthetic data using the GAN generator
+def generate_synthetic_data(num_samples):
+    noise = np.random.normal(0, 1, size=[num_samples, latent_dim])
+    generated_data = generator.predict(noise)
+    return generated_data
+
+# Generate synthetic data
+synthetic_data = generate_synthetic_data(len(X_test))
+
+# Reshape the synthetic_data to 2D array before inverse_transform
+synthetic_data_2d = synthetic_data.reshape(-1, features.shape[1])
+
+# Denormalize the data using the MinMaxScaler's inverse_transform method
+synthetic_data_denormalized = scaler.inverse_transform(synthetic_data_2d)
+
+# Reshape it back to the original shape
+synthetic_data_denormalized = synthetic_data_denormalized.reshape(synthetic_data.shape[0], sequence_length, features.shape[1])
+
+# Predict using the LSTM model on real and synthetic data
+real_predictions = lstm_model.predict(X_test)
+synthetic_predictions = lstm_model.predict(synthetic_data_denormalized)
+
+# Reshape real_predictions and synthetic_predictions to (num_samples,)
+real_predictions = real_predictions.reshape(-1)
+synthetic_predictions = synthetic_predictions.reshape(-1)
+
+# Calculate the min and max values of the original 'Close' prices
+close_min = data['Close'].min()
+close_max = data['Close'].max()
+
+# Denormalize the predictions manually
+real_predictions_denormalized = real_predictions * (close_max - close_min) + close_min
+synthetic_predictions_denormalized = synthetic_predictions * (close_max - close_min) + close_min
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# Get the minimum and maximum values of the original 'Close' prices
+close_min = data['Close'].min()
+close_max = data['Close'].max()
+
+# Evaluate the LSTM model on real data
+actual_close_prices = y_test * (close_max - close_min) + close_min
+
+mse_real = mean_squared_error(actual_close_prices, real_predictions_denormalized)
+mae_real = mean_absolute_error(actual_close_prices, real_predictions_denormalized)
+rmse_real = np.sqrt(mse_real)
+
+print(f"LSTM Model Evaluation on Real Data:")
+print(f"Mean Squared Error (MSE): {mse_real:.4f}")
+print(f"Mean Absolute Error (MAE): {mae_real:.4f}")
+print(f"Root Mean Squared Error (RMSE): {rmse_real:.4f}")
+
+# Evaluate the LSTM model on synthetic data
+mse_synthetic = mean_squared_error(actual_close_prices, synthetic_predictions_denormalized)
+mae_synthetic = mean_absolute_error(actual_close_prices, synthetic_predictions_denormalized)
+rmse_synthetic = np.sqrt(mse_synthetic)
+
+print("\nLSTM Model Evaluation on Synthetic Data:")
+print(f"Mean Squared Error (MSE): {mse_synthetic:.4f}")
+print(f"Mean Absolute Error (MAE): {mae_synthetic:.4f}")
+print(f"Root Mean Squared Error (RMSE): {rmse_synthetic:.4f}")
+
+# Generate some random input sequences for prediction
+num_sequences_to_predict = 500
+random_indices = np.random.randint(0, len(X_test), num_sequences_to_predict)
+input_sequences_to_predict = X_test[random_indices]
+
+# Predict using the LSTM model
+predicted_prices = lstm_model.predict(input_sequences_to_predict)
+
+# Denormalize the predictions manually using the minimum and maximum values of 'Close' prices
+predicted_prices_denormalized = predicted_prices * (close_max - close_min) + close_min
+
+# Plot the predicted prices alongside the actual prices from the test data
+plt.figure(figsize=(12, 6))
+plt.plot(actual_close_prices, label='Actual Close Prices', color='b')
+plt.plot(predicted_prices_denormalized, label='LSTM Predictions', color='r')
+
+plt.title('LSTM Model Predictions')
+plt.xlabel('Time')
+plt.ylabel('GBP/USD Close Price')
+plt.legend()
+plt.grid(True)
+plt.show()
